24322d2686175d1e607df54c8579d301d3929170
rabbi25122010-dotcom <rabbi25122010@gmail.com>
2026-05-24 18:37:03 +0600

    firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,3 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// database কানেকশন তৈরি করে তা EXPORT করা হলো
+export const database = getDatabase(app);
