abaf22f3aa94f554f4a48a25afb172c2436ba065
rabbi25122010-dotcom <rabbi25122010@gmail.com>
2026-05-24 11:30:02 +0600

    firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyOV6Tz7MiTF6Fz-JhbYKqtAmOUOYW7Gk",
+  authDomain: "silver-wolf-organization.firebaseapp.com",
+  databaseURL: "https://silver-wolf-organization-default-rtdb.firebaseio.com",
+  projectId: "silver-wolf-organization",
+  storageBucket: "silver-wolf-organization.firebasestorage.app",
+  messagingSenderId: "96125476768",
+  appId: "1:96125476768:web:dd045d87faa8ae0130cec5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
